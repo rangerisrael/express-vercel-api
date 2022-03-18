f9cc460734657ae9910ff130e641fe74c3b016c8
bryan sumague <israelalisoso1@gmail.com>
2022-03-18 13:41:41 +0800

    Test api

--- api/index.js
+++ api/index.js
@@ -12,17 +12,17 @@ const {
 
 const db = './db.json';
 
-router.get('/', async (req, res) => {
-	try {
-		res.json({
-			status: 200,
-			message: 'Get data has successfully',
-		});
-	} catch (error) {
-		console.error(error);
-		return res.status(500).send('Server error');
-	}
-});
+// router.get('/', async (req, res) => {
+// 	try {
+// 		res.json({
+// 			status: 200,
+// 			message: 'Get data has successfully',
+// 		});
+// 	} catch (error) {
+// 		console.error(error);
+// 		return res.status(500).send('Server error');
+// 	}
+// });
 
 router.get('/', (req, res, next) => {
 	get(db, function (data) {
