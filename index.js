6d06031a9b76f526434b12c3b5e494275b567dfa
bryan sumague <israelalisoso1@gmail.com>
2022-03-18 13:19:43 +0800

    Fixed

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const product = require("./api/product");
+const services = require('./api/index.js');
 
 app.use(express.json({ extended: false }));
 
-app.use("/api/product", product);
+app.use('/api/services', services);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
